953a283c7240ac05835433ec795d20d5819e3f42
usman2019034979 <96358214+usman2019034979@users.noreply.github.com>
2024-09-23 13:10:23 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,5 +7,6 @@
 require_once get_template_directory() . '/includes/posttype.php';
 require_once get_template_directory() . '/includes/metabox.php';
 require_once get_template_directory() . '/includes/theme-settings.php';
+require_once get_template_directory() . '/includes/customizer.php';
 require_once get_template_directory() . '/includes/shortcodes.php';
 require_once get_template_directory() . '/includes/widgets.php';
